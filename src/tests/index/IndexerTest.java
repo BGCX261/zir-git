ba68d22703e3ba8799136d5262d56ca4f6cd1cc8
Ameer Tawfik <ameertawfik>
2013-03-03 18:57:10 +0800

    Adding Indexer tests: testing add method testing index method

---
+++ src/tests/index/IndexerTest.java
@@ -0,0 +1,59 @@
+package tests.index;
+
+import static org.junit.Assert.*;
+import index.Indexer;
+import index.PostingEntry;
+import index.Tokenizer;
+
+import org.junit.Test;
+
+public class IndexerTest {
+
+	@Test
+	public void add() {
+		//two things should take place: the term is added and the docID is added to its list.
+		// if a term is not new, then its posting should contains both docIDs.
+		
+		int docID1 = 1, docID2= 2;
+		String word1 = "ameer", word2 = "ameer";
+		
+		Indexer indexer = new Indexer();
+		indexer.add(word1, docID1);
+		assertEquals("Testing adding a new term",true, indexer.getPosting(word1)!=null);
+		assertEquals("Testing adding docID to the new term posting", 
+				true, 
+				indexer.getPosting(word1).getDocList().contains(new PostingEntry(docID1)));
+		
+		indexer.add(word1, docID2);
+		assertEquals("Testing adding docID to the existing term posting", 
+				true, 
+				indexer.getPosting(word1).getDocList().contains(new PostingEntry(docID2)));
+		assertEquals("Testing adding docID to the existing term posting", 
+				true, 
+				indexer.getPosting(word1).getDocList().size() == 2);
+	}
+	
+	@Test
+	public void index()
+	{
+		String d1 = "A simple text";
+		String d2 = "Another text";
+		
+		
+		Indexer indexer = new Indexer();
+		indexer.index(d1);
+		indexer.index(d2);
+		
+		// to test accuracy: the words following should have been added: "a", "simple", "text" and "another"
+		
+		assertEquals(true, indexer.getPosting("A") != null);
+		assertEquals(true, indexer.getPosting("simple") != null);
+		assertEquals(true, indexer.getPosting("text") !=null);
+		assertEquals(true, indexer.getPosting("Another") !=null);
+		assertEquals(false,indexer.getPosting("nothing") !=null);
+		
+		assertEquals(2, indexer.getPosting("text").getDocList().size());// the word "text" should have two posting entries
+ 
+	}
+
+}
